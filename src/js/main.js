f53b465d092735dd84d4f3d92a787ae3a2050770
mariisena <marianasena@sempreceub.com>
2026-05-26 23:33:30 -0300

    remove(tweaks-panel): remover painel de personalização

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import ThemeToggle from "./components/theme-toggle.js";
 import AcessoRapido from "./components/quick-access.js";
 import MenuToggle from "./components/menu-toggle.js";
-import ThemeTweaker from "./components/tweak-theme.js";
 
 new ThemeToggle();
 new AcessoRapido();
@@ -12,6 +11,4 @@ export {
     ThemeToggle,
     AcessoRapido,
     MenuToggle,
-    ThemeTweaker
-
 };
